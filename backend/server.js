9a487a36c60a5392642728880c1831ff1bebeded
Akshitha <kanchiakshitha444@gmail.com>
2026-04-16 11:42:57 +0530

    Fix Express wildcard route for v5

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use('/api/chat', chatRoutes);
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
+app.use((req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
